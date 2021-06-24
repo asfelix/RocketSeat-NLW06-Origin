9debcb2834a2b6b8e724760467b927bb60713ab1
Alexsandro Felix <felix@ffelix.eti.br>
2021-06-24 00:15:10 -0300

    Conclusão da aula 02

---
+++ README.md
@@ -0,0 +1,8 @@
+links:
+* https://www.notion.so/Mission-Origin-003847efca1f4d8f938beb5c4f6a3578
+
+* https://www.figma.com/file/tPaWiohbPABg9BoT6KIaJi/Origin-Six-(Copy)?node-id=104%3A0
+
+* https://www.iconfinder.com/
+
+* https://icomoon.io/app/
---
+++ js/main.js
@@ -0,0 +1,32 @@
+/* abre e fecha menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do meu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= navHeight) {
+        /* scroll maior que altura do header */
+        header.classList.add('scroll')
+    } else {
+        /* scroll menor que altura do header */
+        header.classList.remove('scroll')
+    }
+})
